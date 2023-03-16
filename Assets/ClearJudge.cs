98bdfc89f60969c01f44b93cb9a252db0ebfde40
umenokoudai <umenokoudai0805@gmail.com>
2023-03-16 19:39:31 +0900

    ボタン入力とクリア判定のスクリプト作成

---
+++ Assets/ClearJudge.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ClearJudge : MonoBehaviour
+{
+    string[][] _judgeNum = { new string[] { "","",""}, new string[] {"","",""}, new string[] {"","",""} };
+    public string[][] JudeNum { get => _judgeNum; set => _judgeNum = value; }
+    bool _isEnd;
+
+
+    public void Judge()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if (string.Join("", _judgeNum[i]) == "ZZZ")
+            {
+                _isEnd = true;
+                Debug.Log("Z‚ÌŸ—˜");
+            }
+            if(string.Join("", _judgeNum[i]) == "~~~")
+            {
+                _isEnd = true;
+                Debug.Log("~‚ÌŸ—˜");
+            }
+        }
+        if (!_isEnd)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                string[] vertical = Enumerable.Repeat("",3).ToArray();
+                for (int j = 0; j < 3; j++)
+                {
+                    vertical[i] = _judgeNum[j][i];
+                }
+                if (string.Join("", vertical) == "ZZZ")
+                {
+                    _isEnd = true;
+                    Debug.Log("Z‚ÌŸ—˜");
+                }
+                if (string.Join("", vertical) == "~~~")
+                {
+                    _isEnd = true;
+                    Debug.Log("~‚ÌŸ—˜");
+                }
+            }
+        }
+        if(!_isEnd)
+        {
+            string[] leftUp = Enumerable.Repeat("", 3).ToArray();
+            string[] rightUp = Enumerable.Repeat("", 3).ToArray();
+            for(int i = 0; i < 3; i++)
+            {
+                leftUp[i] = _judgeNum[i][i];
+                rightUp[i] = _judgeNum[i][3 - i];
+            }
+            if (string.Join("", leftUp) == "ZZZ" || string.Join("", rightUp) == "ZZZ")
+            {
+                Debug.Log("Z‚ÌŸ—˜");
+            }
+            if (string.Join("", leftUp) == "~~~" || string.Join("", rightUp) == "~~~")
+            {
+                Debug.Log("~‚ÌŸ—˜");
+            }
+        }
+    }
+}
---
+++ Assets/SelectButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class SelectButton : MonoBehaviour
+{
+    [SerializeField] bool _maru;
+    [SerializeField] bool _batu;
+    EventSystem _eventSystem;
+
+    public void PushButton(string s)
+    {
+        int[] nums = Array.ConvertAll(s.Split(), int.Parse);
+        _eventSystem = EventSystem.current;
+        GameObject selectButton = _eventSystem.currentSelectedGameObject;
+        if(_maru)
+        {
+            for(int i = 0; i < selectButton.gameObject.transform.childCount; i++)
+            {
+                selectButton.gameObject.transform.GetChild(i).gameObject.SetActive(false);
+            }
+            selectButton.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+        if(_batu)
+        {
+            for (int i = 0; i < selectButton.gameObject.transform.childCount; i++)
+            {
+                selectButton.gameObject.transform.GetChild(i).gameObject.SetActive(false);
+            }
+            selectButton.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        }
+    }
+
+}
